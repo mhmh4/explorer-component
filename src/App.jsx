ac7be825188cf1836d6ca5b2a8c57adb79f4abf5
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-11-26 15:01:17 -0500

    Add state for files

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import { useEffect, useState } from "react";
+
 export default function App() {
-  return "<App />";
+  const [files, setFiles] = useState([]);
+
+  useEffect(() => {
+    setFiles(["file 1", "file 2", "file 3"]);
+  }, []);
+
+  return (
+    <>
+      {files.map((file, index) => {
+        return <div key={index}>{file}</div>;
+      })}
+    </>
+  );
 }
