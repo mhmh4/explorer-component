541b763ac1ec8bad7bb002385985061d8121a93d
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-11-27 08:34:43 -0500

    Let nodes either be a file or directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,39 @@
 import { useState } from "react";
 
-function Node() {
+function Node({ isDirectory }) {
   const [name, setName] = useState("name");
   const [children, setChildren] = useState([]);
 
   return (
     <>
       <span className="mr-4">{name}</span>
-      <button
-        onClick={() => {
-          setChildren((currentChildren) => {
-            return [
-              ...currentChildren,
-              "file " + crypto.randomUUID().substring(0, 8),
-            ];
-          });
-        }}
-      >
-        Add new file
-      </button>
-      <button
-        onClick={() => {
-          setChildren((currentChildren) => {
-            return [...currentChildren, <Node />];
-          });
-        }}
-      >
-        Add new directory
-      </button>
-      <div className="border-l border-black pl-4">
-        {children.map((item, index) => {
-          return <div key={index}>{item}</div>;
-        })}
-      </div>
+      {isDirectory && (
+        <div>
+          <button
+            onClick={() => {
+              setChildren((currentChildren) => {
+                return [...currentChildren, <Node isDirectory={false} />];
+              });
+            }}
+          >
+            Add new file
+          </button>
+          <button
+            onClick={() => {
+              setChildren((currentChildren) => {
+                return [...currentChildren, <Node isDirectory={true} />];
+              });
+            }}
+          >
+            Add new directory
+          </button>
+          <div className="border-l border-black pl-4">
+            {children.map((item, index) => {
+              return <div key={index}>{item}</div>;
+            })}
+          </div>
+        </div>
+      )}
     </>
   );
 }
@@ -40,7 +41,7 @@ function Node() {
 export default function App() {
   return (
     <div className="m-10">
-      <Node />
+      <Node isDirectory={true} />
     </div>
   );
 }
